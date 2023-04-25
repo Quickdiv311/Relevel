5729cd3dc8b51248bb0780a986eab13da92e767c
Divyansh Agarwal <dagarwal058@gmail.com>
2023-04-25 19:27:54 +0530

    delete Node

--- 1. DSA/Node DS/Trees/BST/deleteNode.js
+++ 1. DSA/Node DS/Trees/BST/deleteNode.js
@@ -3,61 +3,72 @@ class Node
     constructor(d)
     {
         this.data = d;
-        this.right = null;
         this.left = null;
+        this.right = null;
     }
 }
 
-class BST
+class BST 
 {
-     constructor()
-     {
-         this.root = null;
-     }
-
-     insertHelper(x,root)
-     {
-          if(root==null) {
-            return new Node(x);
-          }
-
-          if(x< root.data)
-          {
-             root.left = this.insertHelper(x,root.left);
-          }
-          else
-          {
-            root.right = this.insertHelper(x,root.right);
-          }
-          return root;
-     }
+    constructor()
+    {
+        this.root = null;
+    }
 
     insert(x)
     {
-        if(this.root==null)
-        {
-            this.root = new Node(x);
-            return;
-        }
-        this.insertHelper(x,this.root);
+        this.root = this.insertHelper(this.root,x);
     }
 
-    removeHelper(x,root)
+    insertHelper(root,x)
+    {
+        if(root == null)
+          return new Node(x);
+
+        if(x < root.data)
+        {
+            root.left = this.insertHelper(root.left,x);
+        }
+        else
+        {
+            root.right = this.insertHelper(root.right,x);
+        }
+        return root;
+    }
+
+    inorder()
+    {
+        this.inorderHelper(this.root);
+    }
+
+    inorderHelper(root)
+    {
+        if(root==null)
+          return;
+
+        this.inorderHelper(root.left);
+        console.log(root.data);
+        this.inorderHelper(root.right);
+    }
+
+    remove(x)
+    {
+        this.removeHelper(this.root,x);
+    }
+
+    removeHelper(root,x)
     {
         if(root==null) return null;
-        
         if(root.data == x)
         {
             if(!root.left && !root.right) return null;
-            
-            if(root.left && !root.right)
+            if(root.left && !root.right) 
             {
                 let temp = root.left;
                 root.left = null;
                 return temp;
             }
-
-            if(!root.left && root.right)
+            if(!root.left && root.right) 
             {
                 let temp = root.right;
                 root.right = null;
@@ -65,54 +76,29 @@ class BST
             }
 
             let temp = root.left;
-            if(!temp.right)
-            {
-                root.left = null;
-            }
-            while(temp.right) {temp = temp.right;}
+            while(temp.right){temp = temp.right;}
             root.data = temp.data;
-            this.removeHelper(temp.data,root.left);
-            return root;
+            root.left = this.removeHelper(root.left,temp.data);
         }
 
-        if(x < root.data)
+        if(root.data < x)
         {
-            root.left = this.removeHelper(x,root.left);
+            root.left = this.removeHelper(root.left,x);
         }
-
         else
         {
-            root.right = this.removeHelper(x,root.right);
+            root.right = this.removeHelper(root.right,x);
         }
-    }
-
-    remove(x)
-    {
-        this.removeHelper(x,this.root);
-    }
-
-    inorderHelper(root)
-    {
-        if(root==null) return;
-
-        this.inorderHelper(root.left);
-        console.log(root.data);
-        this.inorderHelper(root.right);
-    }
-
-    inorder()
-    {
-        this.inorderHelper(this.root);
+        return root;
     }
 }
 
-let a = [10,5,45,19,53];
+let a = [6,4,8,1,9,5];
 let bst = new BST();
 
 for(let i=0;i<a.length;i++)
 {
   bst.insert(a[i]);
 }
-bst.remove(45);
+bst.remove(6);
 bst.inorder();
-
